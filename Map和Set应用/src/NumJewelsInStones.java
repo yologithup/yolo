ab5b30f8ce88aa0dec48de76b40f942a289e9674
yologithup <18821769219@163.com>
2019-10-10 23:41:03 +0800

    面试题

---
+++ Map和Set应用/src/NumJewelsInStones.java
@@ -0,0 +1,24 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author yolo
+ * @date 2019/10/10-23:39
+ */
+public class NumJewelsInStones {
+    public int numJewelsInStones(String J, String S) {
+        // 把所有宝石放到 Set 中
+        Set<Character> jewels = new TreeSet<>();
+        for (char j : J.toCharArray()) {
+            jewels.add(j);
+        }
+        // 遍历所有的石头
+        int count = 0;
+        for (char s : S.toCharArray()) {
+            if (jewels.contains(s)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
