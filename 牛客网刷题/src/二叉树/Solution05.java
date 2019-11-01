9aeab0a8cec1a8aaaf7c782d1a94cc23b382dd4a
yologithup <18821769219@163.com>
2019-11-01 22:24:42 +0800

    输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。

--- 牛客网刷题/src/二叉树/Solution05.java
+++ 牛客网刷题/src/二叉树/Solution05.java
@@ -5,10 +5,15 @@ package 二叉树;
  * @date 2019/11/1-21:47
  * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
  * 如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+ * 左右根
+ * 左子树<根<右子树
+ * 思路：先利用左子树<根找出左右子树的分界点，找出左子树
+ *      再利用根<右子树判断数组后部分是否满足右子树的特点
+ *
  */
 public class Solution05 {
     public boolean VerifySquenceOfBST(int [] sequence) {
-        if(sequence==null||sequence.length==0){
+        if(sequence==null||sequence.length==0){//注意为null的情况，数组为空，或者数组长度为null，均返回false
             return false;
         }
         return isTree(0,sequence.length-1,sequence);
@@ -19,15 +24,15 @@ public class Solution05 {
             return true;
         }
         int root=sequence[end];
-        int index=star;
-        while(index<end&&sequence[index]<root){
-            index++;
+        int split=star;
+        while(split<end&&sequence[split]<root){
+            split++;//找出左子树
         }
-        for(int i=index;i<end;i++){
+        for(int i=split;i<end;i++){
             if(sequence[i]<root){
-                return false;
+                return false;//若不满足根<右子树，则返回false
             }
         }
-        return isTree(star,index-1,sequence)&&isTree(index,end-1,sequence);
+        return isTree(star,split-1,sequence)&&isTree(split,end-1,sequence);
     }
 }
