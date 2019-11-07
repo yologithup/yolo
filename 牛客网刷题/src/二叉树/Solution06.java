f59c8fc9358d1b7ac34b139a24df1cfd894dd697
yologithup <18821769219@163.com>
2019-11-07 16:31:52 +0800

    输入一颗二叉树的跟节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径

--- 牛客网刷题/src/二叉树/Solution06.java
+++ 牛客网刷题/src/二叉树/Solution06.java
@@ -9,7 +9,21 @@ import java.util.ArrayList;
  * 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。(注意: 在返回值的list中，数组长度大的数组靠前)
  */
 public class Solution06 {
-    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+    private ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<Integer> list = new ArrayList<>();
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+        if(root == null)return result;
+        list.add(root.val);
+        target -= root.val;
+        if(target == 0 && root.left == null && root.right == null)
+            result.add(new ArrayList<Integer>(list));
+        FindPath(root.left, target);
+        FindPath(root.right, target);
+        list.remove(list.size()-1);
+        return result;
+    }
+
+    public static void main(String[] args) {
 
     }
 }
