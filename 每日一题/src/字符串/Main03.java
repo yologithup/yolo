5d81f716e3e6686322fc23b8aa922ef57d076e93
yologithup <18821769219@163.com>
2019-11-10 22:30:46 +0800

    在字符串中删除特定的字符

--- 每日一题/src/字符串/Main03.java
+++ 每日一题/src/字符串/Main03.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 /**
  * @author yolo
  * @date 2019/11/10-22:00
+ * 在字符串中删除特定的字符。
+ * 题目：输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。
+ *
+ *  例如，输入”They are students.”和”aeiou”，则删除之后的第一个字符串变成”Thy r stdnts.”。
  */
 public class Main03 {
     public static void main(String[] args) {
