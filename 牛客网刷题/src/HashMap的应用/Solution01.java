2ab530026eba6cd756114433ed35eab9742150b4
yologithup <18821769219@163.com>
2019-11-12 23:48:19 +0800

    线程安全

---
+++ 牛客网刷题/src/HashMap的应用/Solution01.java
@@ -0,0 +1,28 @@
+package HashMap的应用;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yolo
+ * @date 2019/11/12-15:27
+ * 请实现一个函数用来找出字符流中第一个只出现一次的字符。
+ * 例如，当从字符流中只读出前两个字符"go"时，第一个只出现一次的字符是"g"。
+ * 当从该字符流中读出前六个字符“google"时，第一个只出现一次的字符是"l"。
+ * 输出描述:
+ * 如果当前字符流没有存在出现一次的字符，返回#字符。
+ */
+public class Solution01 {
+    char[]str;
+    Map<Character,Integer>map=new HashMap<>();
+    //Insert one char from stringstream
+    public void Insert(char ch) {
+        for (int i = 0; i < str.length; i++) {
+           str[i]=ch;
+        }
+    }
+    //return the first appearence once char in current stringstream
+//    public char FirstAppearingOnce() {
+//
+//    }
+}
