40251e13b34a010ad4d03e9fe39efe375ec30b0b
yologithup <18821769219@163.com>
2019-11-25 21:31:54 +0800

    百万富翁问题

---
+++ 每日一题/src/生活中的算法/Main11.java
@@ -0,0 +1,13 @@
+package 生活中的算法;
+
+/**
+ * @author yolo
+ * @date 2019/11/25-21:18
+ */
+public class Main11 {
+    public static void main(String[] args) {
+        int sum1=10*30;
+        long sum2=(long)Math.pow(2,30)-1;
+        System.out.println("%d,%d",sum1,sum2);
+    }
+}
