d61a9a693b65bb865a430605b8dee5fe13ce45e6
yologithup <18821769219@163.com>
2019-11-27 23:26:06 +0800

    链表分割

--- 每日一题/src/链表/Main02.java
+++ 每日一题/src/链表/Main02.java
@@ -3,6 +3,11 @@ package 链表;
 /**
  * @author yolo
  * @date 2019/11/27-22:08
+ * 题目描述
+ * 编写代码，以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前
+ *
+ * 给定一个链表的头指针 ListNode* pHead，请返回重新排列后的链表的头指针。
+ * 注意：分割以后保持原来的数据顺序不变。
  */
 public class Main02 {
     public ListNode partition(ListNode pHead, int x) {
