045d4d3750820ff08e18d85aa72d468fa2de2e17
yologithup <18821769219@163.com>
2019-12-01 09:51:30 +0800

    合并连个有序链表

--- 测验题/src/第二次/Main02.java
+++ 测验题/src/第二次/Main02.java
@@ -10,11 +10,11 @@ package 第二次;
  *
  * 输入：1->2->4, 1->3->4
  * 输出：1->1->2->3->4->4
- * * public class ListNode {
- *  *     int val;
- *  *     ListNode next;
- *  *     ListNode(int x) { val = x; }
- *  * }
+ * public class ListNode {
+ *      int val;
+ *      ListNode next;
+ *      ListNode(int x) { val = x; }
+ *  }
  */
 public class Main02 {
     public ListNode Merge(ListNode list1,ListNode list2) {
