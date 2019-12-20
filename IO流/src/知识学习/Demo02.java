a05b317b7545b5423d312620a333465bc54c78c5
yologithup <18821769219@163.com>
2019-12-20 15:35:14 +0800

    遍历某一路径下对应的所有文件

--- IO流/src/知识学习/Demo02.java
+++ IO流/src/知识学习/Demo02.java
@@ -22,11 +22,11 @@ public class Demo02 {
 
     public static void listDir(File dir) {
         File[]files=dir.listFiles();//返回一个文件数组，表示由该抽象路径名表示的目录中的文件。
-        for (File file : files) {//遍历文件数组，判断数组中的每个元素时目录还是文件
-            if(file.isDirectory()){
+        for (File file : files) {//遍历文件数组
+            if(file.isDirectory()){//判断数组中的每个元素是否为目录
                 listDir(file);// 是目录继续递归调用函数，
             }else {
-                System.out.println(file.getAbsoluteFile());//是文件直接打印该文件所对应的绝对路径
+                System.out.println(file.getAbsoluteFile()+" :"+file.length());//不是目录，是文件直接打印该文件所对应的绝对路径
             }
         }
 
